50833dd798e5576bcef5d3cf54222dca14e5faf3
Takuto Mouri <m3.p0nga@gmail.com>
2020-09-15 07:05:51 +0900

    修正:セキュリティ対策

--- makehabitsproject/makehabitsproject/settings.py
+++ makehabitsproject/makehabitsproject/settings.py
@@ -25,9 +25,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'soq2c82y*bal-mhp8)y1e2apt1%58n^k*+eg8_6v^-#-vu)=4!'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
