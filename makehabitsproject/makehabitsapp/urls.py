63395c0ab462328d49aa1aa973db57455a91d495
Takuto Mouri <m3.p0nga@gmail.com>
2020-09-20 22:49:47 +0900

    追加:ログイン機能の追加 #6

--- makehabitsproject/makehabitsapp/urls.py
+++ makehabitsproject/makehabitsapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HabitList, HabitDetail, HabitCreate
+from .views import HabitList, HabitDetail, HabitCreate, Account_login
 
 urlpatterns = [
     path('habitList/', HabitList.as_view(), name='habitList'),
     path('habitDetail/<int:pk>', HabitDetail.as_view(), name='habitDetail'),
     path('habitCreate/', HabitCreate.as_view(), name='habitCreate'),
+    path('login/', Account_login.as_view(), name='login'),
 ]
--- makehabitsproject/makehabitsapp/views.py
+++ makehabitsproject/makehabitsapp/views.py
@@ -1,18 +1,43 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, View
 from .models import HabitModel
 from django.urls import reverse_lazy
+from .forms import LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+
 
 class HabitList(ListView):
     template_name = 'habitList.html'
     model = HabitModel
 
+
 class HabitDetail(DetailView):
     template_name = 'habitDetail.html'
     model = HabitModel
+
 
 class HabitCreate(CreateView):
     template_name = 'habitCreate.html'
     model = HabitModel
     fields = ('title', 'memo')
     success_url = reverse_lazy('habitList')
+
+
+class Account_login(View):
+    def post(self, request, *arg, **kwargs):
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            user = User.objects.get(username=username)
+            login(request, user)
+            return redirect('/habitList')
+        else:
+            return render(request, 'login.html', {'form': form, })
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        return render(request, 'login.html', {'form': form, })
+
+
+account_login = Account_login.as_view()
