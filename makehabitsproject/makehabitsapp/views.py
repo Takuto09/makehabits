7a54ccdb97efcfdee3fb219ec7b645d3520fd24c
Takuto Mouri <m3.p0nga@gmail.com>
2020-09-22 00:01:23 +0900

    追加:アカウント登録 #11

--- makehabitsproject/makehabitsapp/views.py
+++ makehabitsproject/makehabitsapp/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView, View
-from .models import HabitModel
-from django.urls import reverse_lazy
-from .forms import LoginForm
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView, View
+
+from .forms import LoginForm, UserCreateForm
+from .models import HabitModel
 
 
 class HabitList(ListView):
@@ -22,6 +23,29 @@ class HabitCreate(CreateView):
     model = HabitModel
     fields = ('title', 'memo')
     success_url = reverse_lazy('habitList')
+
+
+class Create_account(CreateView):
+    def post(self, request, *args, **kwargs):
+        form = UserCreateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            # フォームから'username'を読み取る
+            username = form.cleaned_data.get('username')
+            # フォームから'password1'を読み取る
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/habitList')
+        else:
+            return render(request, 'signup.html', {'form': form, })
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreateForm(request.POST)
+        return render(request, 'signup.html', {'form': form, })
+
+
+create_account = Create_account.as_view()
 
 
 class Account_login(View):
