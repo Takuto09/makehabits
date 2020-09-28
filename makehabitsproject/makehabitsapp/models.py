3d2e8ea8d6aebe5ca2b1001538d54a775266fc90
Takuto Mouri <m3.p0nga@gmail.com>
2020-09-29 08:16:42 +0900

    追加:モデルの作成 #25

--- makehabitsproject/makehabitsapp/models.py
+++ makehabitsproject/makehabitsapp/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 
 class HabitModel(models.Model):
+    # 登録する習慣の内容
     user_id = models.CharField(max_length=100, default='0')
+    habit_id = models.CharField(max_length=100, default='0')
     title = models.CharField(max_length=100)
     memo = models.TextField()
 
     def __str__(self):
         return self.title
+
+
+class HabitDetailModel(models.Model):
+    # 登録する習慣の実績
+    user_id = models.CharField(max_length=100)
+    habit_id = models.CharField(max_length=100)
+    created_date = models.DateField(auto_now_add=True)
