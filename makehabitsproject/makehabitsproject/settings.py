e7927a0391c9e6b98d4080b30c05e116352db6cd
Takuto Mouri <m3.p0nga@gmail.com>
2020-10-28 21:28:10 +0900

    追加:MySqlへの接続 close #50

--- makehabitsproject/makehabitsproject/settings.py
+++ makehabitsproject/makehabitsproject/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'makehabitsproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'habit_app', # プロジェクトで使用するデータベース名
+        'USER': 'root', # パソコンにインストールしたMySQLのユーザー名
+        'PASSWORD': '', # 同上。そのパスワード
     }
 }
 
